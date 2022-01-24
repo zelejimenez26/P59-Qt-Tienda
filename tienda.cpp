ec55062b1598d3c5da51246dabc153b3153f60d9
Zelena Jimenez <zjimenezh@est.ups.edu.ec>
2022-01-24 16:02:26 -0600

    Datos iniciales de GUI

--- tienda.cpp
+++ tienda.cpp
@@ -6,10 +6,32 @@ Tienda::Tienda(QWidget *parent)
     , ui(new Ui::Tienda)
 {
     ui->setupUi(this);
+    //Lista de productos en el constructor
+    cargarProductos();
+    //Mostrar los productos en el combo box
+    foreach(Producto *p, m_productos){
+        ui->inProducto->addItem(p->nombre());
+    }
+    //Configurara cabecera de la tabla
+    QStringList cabecera ={"Cantidad", "Producto", "Subtotal"};
+    ui->outDetalle->setColumnCount(3);
+    ui->outDetalle->setHorizontalHeaderLabels(cabecera);
+
 }
 
 Tienda::~Tienda()
 {
     delete ui;
+}
+/**
+ * @brief Tienda::cargarProductos Carga la lista de productos de la tienda
+ */
+void Tienda::cargarProductos()
+{
+    //Crear productos quemados en el codigo
+    m_productos.append(new Producto(1, "Leche", 0.80));
+    m_productos.append(new Producto(2, "Pan", 0.15));
+    m_productos.append(new Producto(3, "Queso", 2.50));
+
 }
 
